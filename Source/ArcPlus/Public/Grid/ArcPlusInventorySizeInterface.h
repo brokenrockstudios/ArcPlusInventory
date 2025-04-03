b4e09aef1b95ad51a52c772132de02febfcaa25d
namrog84 <namrog84@gmail.com>
2025-04-02 23:01:35 -0700

    Add comment and missing default implementation for item size interface

--- Source/ArcPlus/Public/Grid/ArcPlusInventorySizeInterface.h
+++ Source/ArcPlus/Public/Grid/ArcPlusInventorySizeInterface.h
@@ -14,15 +14,14 @@ class UArcPlusInventorySizeInterface : public UInterface
 };
 
 /**
- * 
+ * Add this to your item class to provide a more performant lookup for item size.
+ * Perhaps by implementing a cached size fragment lookup.  
  */
 class ARCPLUSINVENTORY_API IArcPlusInventorySizeInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	UFUNCTION()
-	virtual FVector2D GetItemSize() const;
-	
+	virtual FVector2D GetItemSize() const { return FVector2D::One(); }
 };
